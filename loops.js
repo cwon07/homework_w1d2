07319e7213911b67b5773f4a207ecb9d39c34bf9
Christine Wong <jumiwong@Christines-MacBook-Air.local>
2023-05-03 00:25:56 -0700

    finished loop practice

---
+++ loops.js
@@ -0,0 +1,49 @@
+// I did all 9 for practice, please pick 5 from below :)
+
+// Create a loop that logs the numbers from 0-99 (ascending)
+/*for (let i = 0; i < 100; i++){
+    console.log(i);
+}*/
+
+// Create a loop that logs the numbers from 99-0 (descending)
+/*for (let i = 99; i >= 0; i--){
+    console.log(i);
+}*/
+
+// Create a loop that logs the EVEN numbers from 0-98 (ascending)
+/*for (let i = 0; i < 99; i +=2){
+    console.log(i);
+}*/
+
+// Create a loop that logs the EVEN numbers from 98-0 (descending)
+/*for (let i = 98; i >= 0; i -=2){
+    console.log(i);
+}*/
+
+// Create a loop that logs the ODD numbers from 0-99 (ascending)
+/*for (let i = 1; i < 100; i +=2){
+    console.log(i);
+}*/
+
+// Create a loop that logs the ODD numbers from 99-0 (descending)
+/*for (let i = 99; i >=0; i -=2){
+    console.log(i);
+}*/
+
+// Create a loop that logs the numbers from 49-72 (ascending)
+/*for (let i = 48; i <= 72; i++){
+    console.log(i)
+}*/
+
+// Create a loop that logs the numbers from 81-26 (descending)
+/*for (let i = 81; i >= 26; i--){
+    console.log(i)
+}*/
+
+// Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+/*for (let i = 3; i <= 90; i++){
+    if(i % 3=== 0) {
+    console.log(i);
+}
+}*/
+
